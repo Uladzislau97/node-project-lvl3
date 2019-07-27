f261c28c0ed9dfc422fe5ff4a0e597932eec1bcc
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-07-27 23:43:08 +0300

    feat(page-loader): add output param to loadPage

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -6,7 +6,7 @@ program
   .name('page-loader-js')
   .version('1.0.0')
   .arguments('<url>')
-  .action(url => loadPage(url))
+  .action(url => loadPage(url, program.output))
   .description('Download the specified address from the network.')
   .option('-o, --output [path]', 'downloading path')
   .parse(process.argv);
