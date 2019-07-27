00259a7429e5d47d2e80e4747ba9b4a2af316e28
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-07-27 23:42:09 +0300

    refactor(tests): make small changes

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -1,29 +1,33 @@
 import nock from 'nock';
+import axios from 'axios';
 import os from 'os';
 import { promises as fs } from 'fs';
 import path from 'path';
 import loadPage from '..';
 
 const hostname = 'https://hexlet.io';
-const requestPath = '/courses';
-const requestUrl = `${hostname}${requestPath}`;
-const tmpDir = os.tmpdir();
-
-beforeEach(() => {
-  const expectedFilePath = path.resolve(__dirname, '__fixtures__/test_1.html');
-  nock(hostname)
-    .get(requestPath)
-    .replyWithFile(200, expectedFilePath, {
-      'Content-Type': 'text/html',
-    });
-});
+axios.defaults.baseURL = hostname;
 
 test('save file by specified path', async () => {
+  const requestPath = '/courses';
+  const filepath = path.resolve(__dirname, '__fixtures__/test_1.html');
+
+  nock(hostname)
+    .get(requestPath)
+    .replyWithFile(200, filepath, {
+      'Content-Type': 'text/html',
+    });
+
+  const tmpDir = os.tmpdir();
+  const requestUrl = `${hostname}${requestPath}`;
   const pathForSave = await fs.mkdtemp(`${tmpDir}${path.sep}`);
+
   await loadPage(requestUrl, pathForSave);
+
   const actualFilePath = path.resolve(__dirname, pathForSave);
   const actualContent = await fs.readFile(actualFilePath, 'utf8');
   const expectedFilePath = path.resolve(__dirname, '__fixtures__/test_1.html');
   const expectedContent = await fs.readFile(expectedFilePath, 'utf8');
+
   expect(actualContent).toEqual(expectedContent);
 });
