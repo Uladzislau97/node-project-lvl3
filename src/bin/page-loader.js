b70170ab412ae7434d0f9c8054233dee0b8a8b4e
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-07-30 23:47:44 +0300

    feat(page-loader): disable node warnings and update output path resolving

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -1,12 +1,18 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-warnings
 import program from 'commander';
-import loadPage from '..';
+import path from 'path';
+import loadPageByPath from '..';
 
 program
   .name('page-loader-js')
   .version('1.0.0')
   .arguments('<url>')
-  .action(url => loadPage(url, program.output))
+  .action((url) => {
+    const outputPath = program.output
+      ? path.resolve(program.output)
+      : process.cwd();
+    loadPageByPath(url, outputPath).catch(console.log);
+  })
   .description('Download the specified address from the network.')
   .option('-o, --output [path]', 'downloading path')
   .parse(process.argv);
