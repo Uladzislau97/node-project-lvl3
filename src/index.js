b3a2a738922bde2b8d85455cd5e9dd8fdfe0362b
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-07-30 23:47:02 +0300

    feat(page-loader): add the implementation of page loading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const loadPage = (url) => {
+import axios from 'axios';
+import { promises as fs } from 'fs';
+import path from 'path';
+import url from 'url';
 
-};
+const loadPageByPath = (downloadUrl, outputPath) => axios
+  .get(downloadUrl)
+  .then(({ data }) => {
+    const downloadUrlData = url.parse(downloadUrl);
+    const host = downloadUrlData.host.replace(/[./_:]/g, '-');
+    const pathname = downloadUrlData.pathname.replace(/[./_:]/g, '-');
+    const outputFileName = `${host}${pathname}.html`;
+    const outputFilePath = path.resolve(outputPath, outputFileName);
+    return fs.writeFile(outputFilePath, data);
+  });
 
-export default loadPage;
+export default loadPageByPath;
