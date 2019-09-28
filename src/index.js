5d28dcb34d76e4e0350a31141351f864c5f79b65
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2019-09-28 21:47:19 +0300

    feat(index): create structure of file object

--- src/index.js
+++ src/index.js
@@ -2,18 +2,76 @@ import axios from 'axios';
 import { promises as fs } from 'fs';
 import path from 'path';
 import url from 'url';
+import cheerio from 'cheerio';
 
-const loadPageByPath = (address, outputPath) => axios
-  .get(address)
-  .then(({ data }) => {
-    const notLettersOrNumbersRegex = /[\W_]/g;
-    const { protocol } = url.parse(address);
-    const outputFileName = address
-      .replace(`${protocol}//`, '')
-      .replace(notLettersOrNumbersRegex, '-');
-    const fullOutputFileName = `${outputFileName}.html`;
-    const outputFilePath = path.resolve(outputPath, fullOutputFileName);
-    return fs.writeFile(outputFilePath, data);
-  });
+const notLettersOrNumbersRegex = /[\W_]/g;
+
+const buildName = filepath => filepath
+  .split(notLettersOrNumbersRegex)
+  .filter(str => str)
+  .join('-');
+
+const buildFileObject = (address, outputPath) => {
+  const { protocol, ext } = url.parse(address);
+  const filepath = address.replace(`${protocol}//`, '');
+  const outputFileName = buildName(filepath);
+  return {
+    type: 'file',
+    name: outputFileName,
+    outputPath,
+    address,
+    ext,
+  };
+};
+
+const buildResourceObject = (element) => {
+  const tagName = element.prop('tagName');
+  const tag = tagName === 'LINK'
+    ? { name: tagName, attrName: 'href', attrValue: element.attr('href') }
+    : { name: tagName, attrName: 'src', attrValue: element.attr('src') };
+  return { tag };
+};
+
+const isLocalResourceObject = ({ tag }) => !url.parse(tag.attrValue).protocol;
+
+const buildLocalResourceObject = ({ tag }, fileObject) => {
+  const resourceAddress = url.resolve(fileObject.address, tag.attrValue);
+  const { path: resoursePath } = url.parse(resourceAddress);
+  const { dir, name: filename, ext } = path.parse(resoursePath);
+  const filepath = `${dir}/${filename}`;
+  const resourceName = buildName(filepath);
+  const outputPath = path.resolve(fileObject.outputPath, `${fileObject.name}_files/`);
+  const type = tag.name === 'IMG' ? 'bin' : 'file';
+  return {
+    name: resourceName,
+    address: resourceAddress,
+    outputPath,
+    ext,
+    type,
+    tag,
+  };
+};
+
+const loadPageByPath = (address, outputPath) => {
+  const fileObject = buildFileObject(address, outputPath);
+  axios
+    .get(address)
+    .then(({ data }) => {
+      const $ = cheerio.load(data);
+      const $resourceElements = $('link, img, script')
+        .toArray()
+        .map(element => $(element))
+        .filter($element => $element.attr('src') || $element.attr('href'));
+      const resourceObjects = $resourceElements.map(buildResourceObject);
+      const localResourceObjects = resourceObjects
+        .filter(isLocalResourceObject)
+        .map(obj => buildLocalResourceObject(obj, fileObject));
+
+      fileObject.data = data;
+      fileObject.localResources = localResourceObjects;
+      return fileObject;
+    })
+    .then(values => console.log(values));
+};
 
 export default loadPageByPath;
